524d39c79cbff81dfb593adde33b073c6596df74
Arif Rachman Husen <arif.rachman@sicepat.com>
2022-12-16 01:25:32 +0700

    1

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,89 +1,99 @@
-import React, { useEffect, useState } from "react"
-import Button from "../../components/Button"
-import Card from "../../components/Card"
-import emptyDashboardImage from "../../assets/image/activity-empty-state.svg"
-import useDashboard from "./useDashboard"
-import Loading from "../../components/Loading"
-import ModalDelete from "../../components/Modal/ModalDelete"
-import ModalInformation from "../../components/Modal/ModalInformation"
+import React, { useEffect, useState } from "react";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+import emptyDashboardImage from "../../assets/image/activity-empty-state.svg";
+import useDashboard from "./useDashboard";
+import Loading from "../../components/Loading";
+import ModalDelete from "../../components/Modal/ModalDelete";
+import ModalInformation from "../../components/Modal/ModalInformation";
 
 const Dashboard = () => {
-    const {
-        getListActivity,
-        createActivity,
-        handleCancelDelete,
-        deleteActivity,
-        showModal,
-        handleDelete,
-        activity,
-        activities,
-        isLoading,
-        modalInfo,
-        setModalInfo
-    } = useDashboard()
-    useEffect(() => {
-        getListActivity()
-    }, [])
+  const {
+    getListActivity,
+    createActivity,
+    handleCancelDelete,
+    deleteActivity,
+    showModal,
+    handleDelete,
+    activity,
+    activities,
+    isLoading,
+    modalInfo,
+    setModalInfo,
+  } = useDashboard();
+  useEffect(() => {
+    getListActivity();
+  }, []);
 
-    return (
-        <>
-            {isLoading ? (
-                <Loading />
-            ) : (
-                <div className='container'>
-                    <div style={{ margin: "43px 0 55px" }} className='flex flex-row justify-between'>
-                        <h1 data-cy='activity-title' className='text-4xl font-bold'>
-                            Activity
-                        </h1>
-                        <Button dataCy='activity-add-button' text='Tambah' onClick={createActivity} />
-                    </div>
-                    {activities?.length ? (
-                        <div className='dasboard-content w-full'>
-                            <div className='row flex flex-wrap gap-5 '>
-                                {activities.map((item) => {
-                                    return (
-                                        <Card
-                                            key={item.id}
-                                            activity={item}
-                                            handleClickDelete={() => handleDelete(item)}
-                                        />
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    ) : (
-                        <div className='flex items-center w-full'>
-                            <img
-                                style={{ margin: "0 auto" }}
-                                className='align-middle'
-                                src={emptyDashboardImage}
-                                alt='empty-dashboard-image'
-                            />
-                        </div>
-                    )}
-                </div>
-            )}
-            <div data-cy='modal-delete'>
-                {showModal && (
-                    <ModalDelete
-                        actionCancelDelete={handleCancelDelete}
-                        actionDelete={deleteActivity}
-                        text='activity'
-                        activity={activity}
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="container">
+          <div
+            style={{ margin: "43px 0 55px" }}
+            className="flex flex-row justify-between"
+          >
+            <h1 data-cy="activity-title" className="text-4xl font-bold">
+              Activity
+            </h1>
+            <Button
+              dataCy="activity-add-button"
+              text="Tambah"
+              onClick={createActivity}
+            />
+          </div>
+          {activities?.length ? (
+            <div className="dasboard-content w-full">
+              <div className="row flex flex-wrap gap-5 ">
+                {activities.map((item) => {
+                  return (
+                    <Card
+                      key={item.id}
+                      activity={item}
+                      handleClickDelete={() => handleDelete(item)}
                     />
-                )}
+                  );
+                })}
+              </div>
             </div>
-            <div data-cy='modal-information'>
-                {modalInfo && (
-                    <ModalInformation
-                        modalInfo={modalInfo}
-                        setModalInfo={setModalInfo}
-                        text='activity berhasil dihapus'
-                    />
-                )}
+          ) : (
+            <div
+              className="flex items-center w-full"
+              data-cy="todo-empty-state"
+            >
+              <img
+                style={{ margin: "0 auto" }}
+                className="align-middle"
+                src={emptyDashboardImage}
+                alt="empty-dashboard-image"
+              />
             </div>
-        </>
-    )
-}
+          )}
+        </div>
+      )}
+      <div data-cy="modal-delete">
+        {showModal && (
+          <ModalDelete
+            actionCancelDelete={handleCancelDelete}
+            actionDelete={deleteActivity}
+            text="activity"
+            activity={activity}
+          />
+        )}
+      </div>
+      <div data-cy="modal-information">
+        {modalInfo && (
+          <ModalInformation
+            modalInfo={modalInfo}
+            setModalInfo={setModalInfo}
+            text="activity berhasil dihapus"
+          />
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,124 +1,149 @@
-import React, { useEffect, useState } from "react"
-import Button from "../../components/Button"
-import Card from "../../components/Card"
-import toDoEmpty from "../../assets/image/todo-empty-state.svg"
-import { RiArrowLeftSLine } from "react-icons/ri"
-import { TbPencil, TbArrowsDownUp } from "react-icons/tb"
-import Dropdown from "../../components/Dropdown"
-import List from "../../components/List/List"
-import useDetail from "./useDetail"
-import { useParams } from "react-router-dom"
-import Loading from "../../components/Loading"
-import ModalDelete from "../../components/Modal/ModalDelete"
-import ModalInformation from "../../components/Modal/ModalInformation"
-import ModalAddList from "../../components/Modal/ModalAddList"
+import React, { useEffect, useState } from "react";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+import toDoEmpty from "../../assets/image/todo-empty-state.svg";
+import { RiArrowLeftSLine } from "react-icons/ri";
+import { TbPencil, TbArrowsDownUp } from "react-icons/tb";
+import Dropdown from "../../components/Dropdown";
+import List from "../../components/List/List";
+import useDetail from "./useDetail";
+import { useParams } from "react-router-dom";
+import Loading from "../../components/Loading";
+import ModalDelete from "../../components/Modal/ModalDelete";
+import ModalInformation from "../../components/Modal/ModalInformation";
+import ModalAddList from "../../components/Modal/ModalAddList";
 
 export default function Detail() {
-    const {
-        getListItemTodo,
-        details,
-        isLoading,
-        handleCancelDelete,
-        handleDelete,
-        showModal,
-        deleteItemTodo,
-        modalInfo,
-        setModalInfo,
-        todoItem,
-        addModal,
-        setAddModal,
-        handleCancelAdd,
-        handleSave,
-        isEdit,
-        setIsEdit
-    } = useDetail()
-    const [showDropdown, setShowDropdown] = useState(false)
-    const [todo, setTodo] = useState({})
-    const { id } = useParams()
+  const {
+    getListItemTodo,
+    details,
+    isLoading,
+    handleCancelDelete,
+    handleDelete,
+    showModal,
+    deleteItemTodo,
+    modalInfo,
+    setModalInfo,
+    todoItem,
+    addModal,
+    setAddModal,
+    handleCancelAdd,
+    handleSave,
+    isEdit,
+    setIsEdit,
+  } = useDetail();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [todo, setTodo] = useState({});
+  const { id } = useParams();
 
-    useEffect(() => {
-        getListItemTodo(id)
-    }, [])
+  useEffect(() => {
+    getListItemTodo(id);
+  }, []);
 
-    return (
-        <>
-            {isLoading ? (
-                <Loading />
-            ) : (
-                <div className='container '>
-                    <div style={{ margin: "43px 0 55px" }} className='flex flex-row justify-between'>
-                        <div className='flex items-center'>
-                            <RiArrowLeftSLine
-                                data-cy='todo-back-button'
-                                className='font-bold text-[32px] mr-5 cursor-pointer'
-                            />
-                            <h2 data-cy='todo-title' className='text-4xl font-bold'>
-                                {details?.title}
-                            </h2>
-                            <TbPencil
-                                data-cy='todo-title-edit-button'
-                                className='cursor-pointer ml-[27px] text-2xl text-[#888]'
-                            />
-                        </div>
-                        <div className='flex items-center'>
-                            <button
-                                style={{ border: "1px solid #e5e5e5", backgroundClip: "transparent" }}
-                                className='rounded-[50%] w-[54px] h-[54px] mr-[18px]'
-                            >
-                                <TbArrowsDownUp
-                                    onClick={() => {
-                                        setShowDropdown(true)
-                                    }}
-                                    className='cursor-pointer text-2xl text-[#888] inline-block'
-                                />
-                            </button>
-                            <Button onClick={() => setAddModal(true)} dataCy='todo-add-button' text={"Tambah"} />
-                        </div>
-                    </div>
-                    <div className='detail-content'>
-                        {details?.todo_items?.map((value) => {
-                            return (
-                                <List
-                                    details={value}
-                                    handleCancelAdd={handleCancelAdd}
-                                    handleSave={handleSave}
-                                    key={value?.id}
-                                    handleUpdate={() => {
-                                        setTodo(value)
-                                        setIsEdit(true)
-                                        setAddModal(true)
-                                    }}
-                                    text={value?.title}
-                                    handleDelete={() => handleDelete(value)}
-                                />
-                            )
-                        })}
-                    </div>
-                    {/* <div className='flex items-center w-full'>
-                    <img style={{ margin: '0 auto' }} className='align-middle' src={toDoEmpty} alt='empty-dashboard-image' />
-                </div> */}
-                </div>
-            )}
-            {showDropdown ? <Dropdown /> : null}
-            {showModal && (
-                <ModalDelete
-                    actionCancelDelete={handleCancelDelete}
-                    actionDelete={deleteItemTodo}
-                    text='item'
-                    activity={todoItem}
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="container ">
+          <div
+            style={{ margin: "43px 0 55px" }}
+            className="flex flex-row justify-between"
+          >
+            <div className="flex items-center">
+              <RiArrowLeftSLine
+                data-cy="todo-back-button"
+                className="font-bold text-[32px] mr-5 cursor-pointer"
+              />
+              <h2 data-cy="todo-title" className="text-4xl font-bold">
+                {details?.title}
+              </h2>
+              <TbPencil
+                data-cy="todo-title-edit-button"
+                className="cursor-pointer ml-[27px] text-2xl text-[#888]"
+              />
+            </div>
+            <div className="flex items-center">
+              <button
+                style={{
+                  border: "1px solid #e5e5e5",
+                  backgroundClip: "transparent",
+                }}
+                className="rounded-[50%] w-[54px] h-[54px] mr-[18px]"
+              >
+                <TbArrowsDownUp
+                  onClick={() => {
+                    setShowDropdown(true);
+                  }}
+                  className="cursor-pointer text-2xl text-[#888] inline-block"
                 />
-            )}
-            {modalInfo && (
-                <ModalInformation modalInfo={modalInfo} setModalInfo={setModalInfo} text='item berhasil dihapus' />
-            )}
-            {addModal && (
-                <ModalAddList
-                    dataDetail={todo}
-                    isEdit={isEdit}
-                    handleCancel={handleCancelAdd}
+              </button>
+              <Button
+                onClick={() => setAddModal(true)}
+                dataCy="todo-add-button"
+                text={"Tambah"}
+              />
+            </div>
+          </div>
+          {details?.todo_items?.length ? (
+            <div className="detail-content">
+              {details?.todo_items?.map((value) => {
+                return (
+                  <List
+                    details={value}
+                    handleCancelAdd={handleCancelAdd}
                     handleSave={handleSave}
-                />
-            )}
-        </>
-    )
+                    key={value?.id}
+                    handleUpdate={() => {
+                      setTodo(value);
+                      setIsEdit(true);
+                      setAddModal(true);
+                    }}
+                    text={value?.title}
+                    handleDelete={() => handleDelete(value)}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <div
+              className="flex items-center w-full"
+              data-cy="todo-empty-state"
+            >
+              <img
+                style={{ margin: "0 auto" }}
+                className="align-middle"
+                src={toDoEmpty}
+                alt="empty-dashboard-image"
+              />
+            </div>
+          )}
+        </div>
+      )}
+      {showDropdown ? <Dropdown /> : null}
+      {showModal && (
+        <ModalDelete
+          actionCancelDelete={handleCancelDelete}
+          actionDelete={deleteItemTodo}
+          text="item"
+          activity={todoItem}
+        />
+      )}
+      {modalInfo && (
+        <ModalInformation
+          modalInfo={modalInfo}
+          setModalInfo={setModalInfo}
+          text="item berhasil dihapus"
+        />
+      )}
+      {addModal && (
+        <ModalAddList
+          dataDetail={todo}
+          isEdit={isEdit}
+          handleCancel={handleCancelAdd}
+          handleSave={handleSave}
+        />
+      )}
+    </>
+  );
 }
