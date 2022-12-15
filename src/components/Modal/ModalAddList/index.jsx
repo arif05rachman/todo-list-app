b708388d40c3504306554339d53ab7507589d583
Arif Rachman Husen <arif.rachman@sicepat.com>
2022-12-16 01:58:00 +0700

    1

--- src/components/Modal/ModalAddList/index.jsx
+++ src/components/Modal/ModalAddList/index.jsx
@@ -27,12 +27,9 @@ const ModalAddList = ({ handleCancel, handleSave, isEdit, dataDetail }) => {
 
   return (
     <>
-      <div
-        data-cy="modal-add"
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-      >
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          {/*content*/}
+          <div data-cy="modal-add"></div>
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-[830px] bg-white outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
